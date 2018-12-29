0e534dacd07c395ad986bc303c5da0e5aca8e4cd
cheese1884 <songspursuit@gmail.com>
2018-12-29 21:35:14 +0100

    recipe Service 1st commit

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,16 @@
+import { EventEmitter } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'a test', 'https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg?itok=aWyDp3CA'),
+    new Recipe('Another test Recipe', 'a test', 'https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg?itok=aWyDp3CA')
+
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+}
