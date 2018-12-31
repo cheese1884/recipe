5bbc23f97da6e45432640ebb1194df6a1f7ac520
cheese1884 <songspursuit@gmail.com>
2018-12-31 22:21:01 +0100

    ingredients display

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,23 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
-  private recipes: Recipe[] = [
-    new Recipe('A test Recipe', 'a test', 'https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg?itok=aWyDp3CA'),
-    new Recipe('Another test Recipe', 'a test', 'https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg?itok=aWyDp3CA')
 
-  ];
+  private recipes: Recipe[] = [
+    new Recipe('Schnitzel', 'tasty Schnitzel - just awesome',
+    'https://natashaskitchen.com/wp-content/uploads/2016/02/Pork-Schnitzel-Recipe-5.jpg',
+    [new Ingredient('Meat', 1),
+     new Ingredient('French Fries', 20)
+     ]),
+
+    new Recipe('Burger', 'Nice Burger',
+    'https://www.seriouseats.com/recipes/images/2015/07/20150702-sous-vide-hamburger-anova-primary-1500x1125.jpg',
+    [new Ingredient('Buns', 2),
+    new Ingredient('Meat', 1)
+    ]),
+];
 
   getRecipes() {
     return this.recipes.slice();
