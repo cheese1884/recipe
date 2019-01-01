1f4a84f05c77692c329a834646748874ff24dfc1
cheese1884 <songspursuit@gmail.com>
2019-01-01 16:28:22 +0100

    ingredients passed to shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -8,19 +11,19 @@ export class RecipeService {
   private recipes: Recipe[] = [
     new Recipe('Schnitzel', 'tasty Schnitzel - just awesome',
     'https://natashaskitchen.com/wp-content/uploads/2016/02/Pork-Schnitzel-Recipe-5.jpg',
-    [new Ingredient('Meat', 1),
-     new Ingredient('French Fries', 20)
-     ]),
+    [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]),
 
     new Recipe('Burger', 'Nice Burger',
-    'https://www.seriouseats.com/recipes/images/2015/07/20150702-sous-vide-hamburger-anova-primary-1500x1125.jpg',
-    [new Ingredient('Buns', 2),
-    new Ingredient('Meat', 1)
-    ]),
+    'https://www.seriouseats.com/recipes/images/2015/07/20150702-sous-vide-hamburger-anova-primary-1500x1125.jpg', [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]),
 ];
 
+constructor(private slService: ShoppingListService) {}
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 
 }
